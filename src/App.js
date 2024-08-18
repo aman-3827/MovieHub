fc88c1dea4958b022fd6d1f9bd253454dd25fc40
Aman kumar <kumaramanddn390@gmail.com>
2024-08-19 01:12:09 +0530

    Deploy Check 1.9

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=cc746e87';
+const API_URL = 'https://www.omdbapi.com/?apikey=cc746e87';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
